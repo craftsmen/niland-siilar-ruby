ef2912301aba0399d342560eb3c1e654eefe461b
Mehdi Lahmam <mehdi@craftsmen.io>
2015-03-27 04:03:33 +0100

    Update `Track` struct by adding missing attributes

--- lib/siilar/client/tracks.rb
+++ lib/siilar/client/tracks.rb
@@ -2,6 +2,15 @@ module Siilar
   class Client
     module Tracks
 
+      # Gets a track.
+      #
+      # @see http://api.siilar.com/1.0/doc/tracks#get-a-track
+      def track(track)
+        response = client.get("1.0/tracks/#{track}")
+
+        Struct::track.new(response)
+      end
+
       # Creates a track.
       #
       # @see http://api.siilar.com/1.0/doc/tracks#create-a-track
@@ -10,15 +19,6 @@ module Siilar
         response = client.post('1.0/tracks', attributes)
 
         Struct::Track.new(response)
-      end
-
-      # Gets a track.
-      #
-      # @see http://api.siilar.com/1.0/doc/tracks#get-a-track
-      def track(track)
-        response = client.get("1.0/tracks/#{track}")
-
-        Struct::track.new(response)
       end
 
       # Updates a track.
--- lib/siilar/struct/track.rb
+++ lib/siilar/struct/track.rb
@@ -2,7 +2,27 @@ module Siilar
   module Struct
 
     class Track < Base
-      attr_accessor :id, :title, :external_id, :isrc
+      attr_accessor :id, :hash, :title, :popularity, :duration, :external_id, :isrc, :waveform_url
+
+      def album
+        @album ||= {}
+      end
+
+      def album=(attrs)
+        if attrs
+          @album = Struct::Album.new(attrs)
+        end
+      end
+
+      def artist
+        @artist ||= {}
+      end
+
+      def artist=(attrs)
+        if attrs
+          @artist = Struct::Artist.new(attrs)
+        end
+      end
     end
   end
 end
