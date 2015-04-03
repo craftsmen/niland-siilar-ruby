ca0079caf1163aebbfd8c84ab2a6f4fe425be0fe
gcolson <leg.colson@gmail.com>
2015-04-03 12:32:06 +0200

    Add `similar-from-external` endpoint support Search for similar tracks from their external_ids

--- spec/siilar/client/search_spec.rb
+++ spec/siilar/client/search_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Siilar::Client, '.search' do
   subject { described_class.new(api_endpoint: 'http://api.siilar', api_key: 'key').search }
 
-  describe '#search' do
+  describe '#similar' do
     before do
       stub_request(:get, %r[/1.0/search]).to_return(read_fixture('search/similar/success.http'))
     end
@@ -19,6 +19,30 @@ describe Siilar::Client, '.search' do
     it 'returns the search results' do
       attributes = { similar_tracks: '1234' }
       result = subject.similar(attributes)
+
+      expect(result).to be_a(Array)
+      expect(result.first).to be_a(Siilar::Struct::Track)
+      expect(result.first.album).to be_a(Siilar::Struct::Album)
+      expect(result.first.artist).to be_a(Siilar::Struct::Artist)
+    end
+  end
+
+  describe '#similar_from_external' do
+    before do
+      stub_request(:get, %r[/1.0/search-from-external]).to_return(read_fixture('search/similar/success.http'))
+    end
+
+    it 'builds the correct request' do
+      attributes = { similar_tracks: '1234' }
+      subject.similar_from_external(attributes)
+
+      expect(WebMock).to have_requested(:get, 'http://api.siilar/1.0/search-from-external?similar_tracks=1234&key=key')
+                          .with(query: attributes)
+    end
+
+    it 'returns the search results' do
+      attributes = { similar_tracks: '1234' }
+      result = subject.similar_from_external(attributes)
 
       expect(result).to be_a(Array)
       expect(result.first).to be_a(Siilar::Struct::Track)
