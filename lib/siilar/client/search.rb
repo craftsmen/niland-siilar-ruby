21a8c9933357d47c02c52b860521d76148d10975
gcolson <leg.colson@gmail.com>
2015-04-03 12:32:06 +0200

    add similar-from-external method to search module to search with external ids

--- lib/siilar/client/search.rb
+++ lib/siilar/client/search.rb
@@ -11,6 +11,16 @@ module Siilar
 
         response.map { |r| Struct::Track.new(r) }
       end
+
+      # Search for a track from external ids
+      #
+      # @see http://api.siilar.com/1.0/doc/search-and-analyze#search-from-external
+      def similar_from_external(query = {})
+        options = { query: query }
+        response = client.get('1.0/search-from-external', options)
+
+        response.map { |r| Struct::Track.new(r) }
+      end
     end
   end
 end
